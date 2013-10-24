3390012c7d2866a35cfb9f45aebacb9c52f84d38
Elsie <nacaratflame@gmail.com>
2013-10-24 19:59:14 +1100

    Update remove.c

--- remove.c
+++ remove.c
@@ -1,16 +1,34 @@
 //R - Remove item
-TDnode *RemoveItem (TDnode *cursor){
-	//cursor becomes cursor->next, old cursor is freed
+TDnode *RemoveItem (TDnode *list, TDnode *cursor){
+	if(list == NULL){ //The Empty Set
+		return;
+	}
+	
 	TDnode *item;
 	item = cursor;
 	
-	cursor = item->prev;
-	cursor->next = item->next;
+	if(item->prev == NULL && item->next == NULL){ //The list has one entry
+		cursor = NULL;
+		list = NULL; //because the list is now empty
+	}
+	//Now, the node is at the beginning or end of a list
+	else if(item->prev != NULL && item->next == NULL){ //At the end of a list
+		cursor = item->prev;
+		cursor->next = NULL;
+	}
+	else if(item->prev == NULL && item->next != NULL){ //at the beginning of a list
+		cursor = item->next;
+		list = cursor;
+		cursor->prev = NULL;
+	}
 	
-	cursor = item->next;
-	cursor->prev = item->prev;
-	
-	free_list( item );
-	
+	else(item->prev != NULL && item->next != NULL){ //Here item is in the middle of a list.
+		cursor = item->prev;
+		cursor->next = item->next;
+		
+		cursor = item->next;
+		cursor->prev = item->prev;
+	}
+	FreeNode( item );
 	return;
 }

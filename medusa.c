0386b927352d48c4924ee03fefd9ca799ba8cf2e
Elsepth <nacaratflame@gmail.com>
2013-10-27 10:39:51 +1100

    IT LIVES
    Unimplemented functions:
    ALL WRITE FUNCTIONS
    Item mode
    Undo
    Search

--- medusa.c
+++ medusa.c
@@ -24,78 +24,302 @@ Help -> Prints Stuff
 Quit -> Quits
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+
 //typedef char* Task;
 //typedef char* Note;
 
 typedef struct item Item;
 struct item {
 	//int data;
-	int id = 0;
-	Item* prev = NULL;
-	Item* next = NULL;
+	int id;
+	Item* prev;
+	Item* next;
 	//Task task;
 	//Note note;
 };
 
+typedef struct list List;
+struct list{
+	Item* head;
+	Item* tail;
+};
+
+
+Item* Link ( Item* i, List* l );
+Item* Unlink (Item* i, List* l ); //basically remove without bells and whistles
+Item* MakeItem ( void );
+
+void FreeList( Item *list );
+void FreeItem( Item *item );
+
+void PrintList (Item* target, List* list, char mode);
 
 int main( void ) {
-	Item* list = NULL;
+	List list;
+	list.head = NULL;
+	list.tail = NULL;
 	Item* target = NULL; //user's cursor
-	Item* new = NULL;
+	Item* ptr = NULL;
 	Item* lonely = NULL; //after unlinked
 	int i;
+	char mode = 'L';
 	for (i=1; i != 0; i++){
 		printf("Enter command (A,F,B,P,L,R,T,D,C,N,S,U,Q, H for Help): ");
 		char ch = getchar(); while( !isalpha(ch) &&( ch != '\n' )) {ch = getchar();} char op = ch;
 		while( ch != '\n' ) {ch = getchar();}
 		switch( op ) {
-			case 'a': case 'A': new = MakeItem(); new->id = i; target = Link(new); break;
+			case 'a': case 'A': 
+				ptr = MakeItem(); ptr->id = i; target = Link(ptr,&list);
+				ptr = NULL;
+				PrintList (target, &list, mode);
+				break;
+			case 'f': case 'F':
+				if (target == NULL){break;}//DO NOTHING
+				if (target->next == NULL){break;}//DO NOTHING
+				target = target->next;
+				PrintList (target, &list, mode);
+				break;
+			case 'b': case 'B':
+				if (target == NULL){break;}//DO NOTHING
+				if (target->prev == NULL){break;}//DO NOTHING
+				target = target->prev;
+				PrintList (target, &list, mode);
+				break;
+			case 'p': case 'P':
+				PrintList (target, &list, mode);
+				break;
+			case 'l': case 'L':
+				PrintList (target, &list, mode);
+				break;
+			case 'r': case 'R':
+				if (target == NULL){break;}//DO NOTHING
+				if (lonely != NULL){FreeItem(lonely); lonely == NULL;}
+				/*cases:
+				Target is head
+				target is tail
+				target is both
+				target is neither
+				*/
+				ptr = target;
+				if (target == list.head && target == list.tail){target = NULL;}
+				//else if (target == list.head){} //not needed - select next
+				else if (target == list.tail){target = target->prev;}
+				else {target = target->next;}
+				
+				lonely = Unlink(ptr,&list);
+				ptr = NULL;
+				PrintList (target, &list, mode);
+				break;
+				
+			case 't': case 'T':
+				PrintList (target, &list, mode);
+				break;
+			case 'd': case 'D':
+				PrintList (target, &list, mode);
+				break;
+			case 'c': case 'C':
+				PrintList (target, &list, mode);
+				break;
+			case 'n': case 'N':
+				PrintList (target, &list, mode);
+				break;
+				
+			case 'u': case 'U':
+				PrintList (target, &list, mode);
+				break;
+				
+			case 's': case 'S':
+				break;
+			case 'h': case 'H':
+				printf("\n");
+				printf(" A - Add item\n" );
+				printf(" F - move Forward\n" );
+				printf(" B - move Back\n" );
+				printf(" P - Print item\n" );
+				printf(" L - List items\n" );
+				printf(" R - Remove item\n");
+				printf(" T - change Task\n");
+				printf(" D - change Date\n");
+				printf(" C - change Class\n");
+				printf(" N - change Notes\n");
+				printf(" S - Search\n");
+				printf(" U - Undo\n");
+				printf(" Q - Quit\n");
+				printf(" H - Help\n");
+				printf("\n");
+				break;
 			
-			
-			case 'q': case 'Q': i=0;
+			case 'q': case 'Q':
+				if (lonely != NULL){FreeItem(lonely);}
+				FreeList (list.head);
+				return 0;
+				break;
+		}
+	//PrintList (target, &list, mode);
+	}
+return 0;
+}
+void PrintList ( Item* target, List* list, char mode ){
+	Item* ptr = NULL;
+	if (mode = 'L'){
+		for (ptr = list->head ; ptr != NULL ; ptr = ptr->next ){
+		if(ptr == target){printf("->");}else{printf("  ");}
+		printf("<Date><Class><%d><Task> \n", ptr->id);
 		}
 	}
-return 0;}
+}
+	
+void FreeList( Item *list ){ //!!!
+  Item *item;
+
+  while( list != NULL ) {
+	item = list;
+	list = list->next;
+//    free( node->task );
+//    free( node->notes );
+//    free( node );
+	FreeItem ( item );
+	}
+}
+
+void FreeItem ( Item *item ){ //!!!
+   //free( item->task );
+   //free( item->notes );
+   free( item );
+}
 
 Item* MakeItem( void ){
 	Item *newItem = (Item *)malloc( sizeof( Item ));
-	if( newItem == NULL ) {
+	/*if( newItem == NULL ) {
 		 printf("Error: could not allocate memory.\n");
 		 exit( 1 );
-	}
+	}*/
+	assert(newItem != NULL);
+	newItem->id = 0;
+	newItem->prev = NULL;
+	newItem->next = NULL;
 	return (newItem);
 }
 
-Item* Link (Item* i, Item* list){
-	x->prev = x->cursor;
-	int *idA = item->data;
-	int *idX;
-	x->hist = 'A';
-	for(x->cursor = x->first; x->cursor != NULL; x->cursor = x->cursor->next ){
-		idX = x->cursor->data;
-		if(idA < idX){
-			//if (x->cursor == x->first){}//insert it at the beginning
-			//else{} //insert it
-			item->next = x->cursor;
-			item->prev = x->cursor->prev;
-			x->cursor->prev = item;
-			if(item->prev != NULL){item->prev->next = item;}
-			else{x->first = item;}
-			x->cursor = item;
-			return;
-		}
+Item* Unlink (Item* i, List* l){
+	assert(i != NULL && l->head !=NULL && l->tail != NULL);
+	/*
+	Cases:
+	item is null
+	item is head and tail
+	item is head
+	item is tail
+	item is neither
+	*/
+	if (i == l->head && i == l->tail){//item is whole list
+		l->head = NULL; l->tail = NULL;
+		i->prev = NULL; i->next = NULL;
+		return i;
 	}
-	//insert it at the end
-	if (x->last != NULL){
-		x->last->next = item;
-		item->prev = x->last;
+	else if (i->prev == NULL && i->next == NULL){ //item is already unlinked
+		return i;
 	}
-	x->last = item;
-	x->cursor = item;
-	if (x->first == NULL){
-	x->first = item;}
+	else if (i == l->head){ //item is head
+		l->head = i->next;
+		i->next->prev = NULL;
+		i->next = NULL;
+		return i;
+	}
+	else if (i == l->tail){ //item is tail
+		l->tail = i->prev;
+		i->prev->next = NULL;
+		i->prev = NULL;
+		return i;
+	}
+	else { //unlink normally
+		assert (i->prev != NULL || i->next != NULL);//neither should be NULL
+		i->prev->next = i->next;
+		i->next->prev = i->prev;
+		i->prev = NULL; i->next = NULL;
+		return i;
+	}
+	//If you get to this point, something has gone wrong.
 	return;
 }
+
+Item* Link (Item* i, List* l){
+	assert( i != NULL );
+	//assert( l->head != NULL );
+	//assert( l->tail != NULL);
+	/*
+	Takes as input the address of an item, the address of a list.
+	Sorts the item into the list.
+	returns the address of the item.
+	*/
+	/*
+	Possible cases:
+	List is empty.
+	Item goes to head.
+	Item goes to tail.
+	Item is somewhere between two other items.
+	*/
+	if (l->head == NULL && l->tail == NULL){ //then item shall be the new list
+		l->head = i; l->tail = i; return i;
+	}
+	else if ( i->id < l->head->id ){ //then item shall be the new head
+		l->head->prev = i; i->next = l->head;
+		l->head = i;
+		return i;
+	}
+	else if ( i->id > l->tail->id ){ //then item shall be the new tail
+		l->tail->next = i; i->prev = l->tail;
+		l->tail = i;
+		return i;
+	}
+	else{ //item cannot be after tail, nor before head
+		Item* ptr = l->head;
+		for (; ptr != NULL ; ptr = ptr->next ){
+			if (i->id < ptr->id){
+				i->next = ptr; i->prev = ptr->prev;
+				ptr->prev->next = i; ptr->prev = i;
+				return i;
+			}
+		}
+		return;
+	}
+	return;
+}
+
+
+/*
+Item* Link (Item* i, List* l){
+	assert(i != NULL && l->head !=NULL && l->tail != NULL);
+	if (l->head == NULL && l->tail == NULL){ //then item shall be the new list
+		l->head = i; l->tail = i; return i;
+	}
+	else if ( i BEFORE l->head ){ //then item shall be the new head
+		l->head->prev = i; i->next = l->head;
+		l->head = i;
+		return i;
+	}
+	else if ( i AFTER l->tail ){ //then item shall be the new tail
+		l->tail->next = i; i->prev = l->tail;
+		l->tail = i;
+		return i;
+	}
+	else{ //item cannot be after tail, nor before head
+		Item* ptr = l->head;
+		for (; ptr != NULL ; ptr = ptr->next ){
+			if (i BEFORE ptr){
+				i->next = ptr; i->prev = ptr->prev;
+				ptr->prev->next = i; ptr->prev = i;
+				return i;
+			}
+		}
+		return;
+	}
+	return;
+}
+*/
 
 
 //Task GetTask( void );
@@ -105,16 +329,13 @@ Item* Link (Item* i, Item* list){
 //void Link ( Item* i ); //puts item into linked list, sorted
 //void Search ( Item* list );
 
-int Bitwrite(int d, int c);
+//int Bitwrite(int d, int c);
 
 
-
-Link ( Item* i, Item* f );
-Unlink (Item* i ); //basically remove without bells and whistles
 
 //places
 
-
+/*
 void main (void){
 //Item* US; //UserSelect
 //Item* SS; //SystemSelect
@@ -196,4 +417,5 @@ Link ( Item* i ){
 }
 
 
+*/
 
